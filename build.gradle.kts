52960dcffca597dd6e5372c544505e4ccb4516ba
tessoir <mrkekovich.official@gmail.com>
2025-05-10 11:15:03 +0300

    build(build.gradle.kts): Use libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
-val exposed_version: String by project
-val h2_version: String by project
-val koin_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val postgres_version: String by project
-
 plugins {
-    kotlin("jvm") version "2.1.10"
-    id("io.ktor.plugin") version "2.3.13"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.shadowjar)
 }
 
 group = "com.example"
@@ -23,23 +17,15 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-cors")
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-auth")
-    implementation("io.ktor:ktor-server-auth-jwt")
-    implementation("io.ktor:ktor-server-host-common")
-    implementation("io.ktor:ktor-server-status-pages")
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("io.insert-koin:koin-ktor:$koin_version")
-    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
-    implementation("io.ktor:ktor-server-websockets")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.bundles.kotlin.libraries)
+    implementation(libs.bundles.ktor.server.core)
+    implementation(libs.bundles.ktor.server.features)
+    implementation(libs.bundles.ktor.client.core)
+    implementation(libs.bundles.ktor.client.features)
+    implementation(libs.bundles.database)
+    implementation(libs.bundles.di)
+    implementation(libs.bundles.swagger)
+    implementation(libs.bundles.logging)
+    implementation(libs.bundles.kverify)
+    testImplementation(libs.bundles.testing)
 }
