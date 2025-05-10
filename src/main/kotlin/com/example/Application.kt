c55d869446acbdce87fba5e1ac5a73d4b4f2ea8a
tessoir <mrkekovich.official@gmail.com>
2025-05-10 11:29:13 +0300

    refactor: Remove example code

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,10 @@
+package com.example
+
+import io.ktor.server.application.Application
+import io.ktor.server.netty.EngineMain
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+@Suppress("UNUSED")
+fun Application.module() {
+}
